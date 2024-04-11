cb08d30858b34ee176f094f4a14d7bee9765762f
Anders <anders@fatiko.no>
2024-04-11 16:51:45 +0200

    NY

--- App.js
+++ App.js
@@ -1,33 +1,26 @@
 import React from 'react';
-import { NativeBaseProvider, extendTheme } from 'native-base';
+import { NativeBaseProvider } from 'native-base';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+// Import your screens here
 import SplashScreen from './src/screens/SplashScreen';
-import HomeScreen from './src/screens/HomeScreen'; // Import your HomeScreen
-// ... import other screens as needed
+// ... and any other screens you have
 
 const Stack = createStackNavigator();
 
-const theme = extendTheme({
-  // Add your customizations here
-});
-
 const App = () => {
   return (
-    <NativeBaseProvider theme={theme}>
+    <NativeBaseProvider>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Splash">
+          {/* Stack.Screen for the SplashScreen */}
           <Stack.Screen
             name="Splash"
             component={SplashScreen}
-            options={{ headerShown: false }} // Hides the header for the splash screen
+            options={{ headerShown: false }}
           />
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            // options for other screens
-          />
-          {/* Add other Stack.Screen components for your other screens */}
+          {/* Other screens will be listed here */}
         </Stack.Navigator>
       </NavigationContainer>
     </NativeBaseProvider>
